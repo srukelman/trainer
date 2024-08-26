493eb9521742e0bd129463694899c5c487749a48
Sean Kelman <seanrkelman@gmail.com>
2024-08-26 01:08:39 -0700

    changes to app, main, package, and lock for strava api changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-import { useState } from 'react'
 import './App.css'
 import { Calendar } from './components/Calendar/Calendar'
+import { StravaAuthenticate } from './components/Strava/StravaAuthenticate'
+import { Route, Routes } from 'react-router-dom'
+import { StravaRedirect } from './components/Strava/StravaRedirect'
 
 function App() {
 
   return (
     <>
-      <Calendar currentDay={null} />
+      <Routes>
+        <Route path=""
+          element={
+            <>
+              <Calendar currentDay={null} />
+              <StravaAuthenticate />
+            </>
+          }
+        />
+        <Route path="/exchange_token" element={<StravaRedirect />} />
+      </Routes>
+      
     </>
   )
 }
