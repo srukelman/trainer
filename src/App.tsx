85c1de83b43fdc1546545ebcffd87e6c4ae4ec0f
Sean Kelman <seanrkelman@gmail.com>
2024-08-26 14:40:12 -0700

    fix: fixed oauth with strava token still doesnt work to get activity data

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import { Calendar } from './components/Calendar/Calendar'
 import { StravaAuthenticate } from './components/Strava/StravaAuthenticate'
 import { Route, Routes } from 'react-router-dom'
 import { StravaRedirect } from './components/Strava/StravaRedirect'
+import { StravaInfo } from './components/Strava/StravaInfo'
 
 function App() {
 
   return (
     <>
       <Routes>
-        <Route path=""
+        <Route path="/"
           element={
             <>
               <Calendar currentDay={null} />
@@ -18,6 +19,7 @@ function App() {
           }
         />
         <Route path="/exchange_token" element={<StravaRedirect />} />
+        <Route path="/stravainfo/:accessToken" element={<StravaInfo />} />
       </Routes>
       
     </>
