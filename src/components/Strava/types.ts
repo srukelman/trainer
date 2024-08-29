c256058c353d5332e6ba89f43cfb61c93b3d96b6
Sean Kelman <seanrkelman@gmail.com>
2024-08-29 16:32:53 -0700

    feat: added getting data since most recent activity in backend

--- src/components/Strava/types.ts
+++ src/components/Strava/types.ts
@@ -1,5 +1,14 @@
 import { FunctionComponent } from "react"
 
+export interface Activity {
+    id: string,
+    name: string,
+    athlete: { id: string },
+    distance: number,
+    moving_time: number,
+    start_date_local: string,
+}
+
 export type StravaProps = { }
 
 export type StravaInfoComponent = FunctionComponent<StravaProps>
